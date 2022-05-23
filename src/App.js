fc0d78319c13e0fe5c5963d97f58d3951c8b3004
NedalNnee <asel1234543210@gmail.com>
2022-05-23 13:20:27 +0300

    fix0

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Expenses from "./componants/Expanse/Expenses";
 import NewExpense from "./componants/NewExpense/NewExpense";
 import {useState} from "react";
-import Chart from "./componants/Chart/Chart";
 
 const dummyItems = [{
     title: "Car ", amount: 1250, date: new Date(2022, 10, 5), id: 0
